d1de224e26c6a59193bb89763ce5e2ade56e9296
Martin Schweizer <tinu.schweizer@gmail.com>
2026-06-30 20:30:27 +0200

    feat: Logo-Link zum Dashboard, Logout-Button, Login-Logo

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ app = create_app()
 
 if __name__ == '__main__':
     debug = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
-    app.run(debug=debug)
+    port = int(os.environ.get('PORT', 5006))
+    app.run(host='0.0.0.0', port=port, debug=debug)
